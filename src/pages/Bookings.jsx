ba990ea25987c1b1339c86bb4afb3af3c90593c5
sheilalg10 <sheilala6c@gmail.com>
2025-06-06 13:55:05 +0200

    style: Users and Bookings

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import HeaderTabs from "../components/booking/headerTabs";
-import TableBookings from './../components/booking/tableBooking';
+import TableBookings from "./../components/booking/tableBooking";
+import styled from "styled-components";
 
 const BookingsPage = () => {
   const [filter, setFilter] = useState("all");
 
   const bookings = useSelector((state) => state.bookings.data);
 
-  const filteredbookings = bookings
-    .filter((booking) => {
-      const status = booking.status?.toLowerCase();
-      if (filter === "all") return true;
-      return status === filter;
-    });
+  const filteredbookings = bookings.filter((booking) => {
+    const status = booking.status?.toLowerCase();
+    if (filter === "all") return true;
+    return status === filter;
+  });
 
   return (
-    <>
-      <HeaderTabs
-        activeTab={filter}
-        onChangeTab={setFilter}       
-      />
-      <TableBookings
-        bookings={filteredbookings}
-      />
-    </>
-  )};
+    <StyledBooking>
+      <HeaderTabs activeTab={filter} onChangeTab={setFilter} />
+      <TableBookings bookings={filteredbookings} />
+    </StyledBooking>
+  );
+};
+
+const StyledBooking = styled.div`
+  overflow-y: scroll;
+`;
 
 export default BookingsPage;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import HeaderTabs from "../components/employees/headerTabs";
 import TableEmployees from "./../components/employees/tableEmployees";
+import styled from "styled-components";
 
 const EmployeesPage = () => {
   const [filter, setFilter] = useState("all");
@@ -15,11 +16,15 @@ const EmployeesPage = () => {
   });
 
   return (
-    <>
+    <StyledUsers>
       <HeaderTabs activeTab={filter} onChangeTab={setFilter} />
       <TableEmployees employees ={filteredemployees} />
-    </>
+    </StyledUsers>
   );
 };
+
+const StyledUsers = styled.div`
+  overflow-y: scroll;
+`;
 
 export default EmployeesPage;
